9c1437687b76bcb8b5a633acb733841e46a6b391
ABHI-xd <94308307+ABHI-xd@users.noreply.github.com>
2021-12-18 22:29:49 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -17,14 +17,4 @@ if HEROKU:
     
 
 
-# NOTE Fill this if you are not deploying on heroku.
-if not HEROKU:
-    API_ID = 14371
-    API_HASH = "e46b6c854d2bf58a0"
-    ARQ_API_KEY = "Get this from @ARQRobot"
-    CHAT_ID = -100546355432
-    DEFAULT_SERVICE = "saavn"  # Must be one of "youtube"/"saavn"
-    BITRATE = 512 # Must be 512/320
 
-# don't make changes below this line
-ARQ_API = "https://grambuilders.tech"
